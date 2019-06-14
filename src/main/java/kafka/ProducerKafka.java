2f58b9374a600025afd3cac6388f00a706facd04
giuseppe <g1u5e7p3>
2019-06-14 17:43:13 +0200

    Simulator

--- src/main/java/kafka/ProducerKafka.java
+++ src/main/java/kafka/ProducerKafka.java
@@ -4,57 +4,49 @@ import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
 import java.util.Properties;
 
 public class ProducerKafka {
 
 
-        public static void produce(String[] pathList, String[] topic) throws IOException {
-            Properties props = new Properties();
-
-            //Assign localhost id
-            props.put("bootstrap.servers", "3.121.202.240:9092");
-
-            //Set acknowledgements for ProducerKafka requests.
-            props.put("acks", "all");
-
-            //If the request fails, the ProducerKafka can automatically retry,
-            props.put("retries", 0);
-
-            //Specify buffer size in config
-            props.put("batch.size", 16384);
-
-            //Reduce the no of requests less than 0
-            props.put("linger.ms", 1);
-
-            //The buffer.memory controls the total amount of memory available to the ProducerKafka for buffering.
-            props.put("buffer.memory", 33554432);
-
-            props.put("key.serializer",
-                    "org.apache.kafka.common.serialization.StringSerializer");
-
-            props.put("value.serializer",
-                    "org.apache.kafka.common.serialization.StringSerializer");
-            props.put("auto.create.topics.enable", true);
+    public static void produce(Producer<String, String> producer, String line) throws IOException {
+        producer.send(new ProducerRecord<String, String>("comments", line));
+    }
 
 
-            //do something
-            for (int x = 0; x < pathList.length; x++) {
+    public static Producer<String, String> setConfig() {
+        //Set properties
+        Properties props = new Properties();
 
-                Producer<String, String> producer = new KafkaProducer<>(props);
-                producer.send(new ProducerRecord<String, String>(topic[x], "prova"));
-                System.out.println();
-                producer.close();
+        //Assign localhost id
+        props.put("bootstrap.servers", "localhost:9092");
 
-            }
+        //Set acknowledgements for ProducerKafka requests.
+        props.put("acks", "all");
 
+        //If the request fails, the ProducerKafka can automatically retry,
+        props.put("retries", 0);
 
-        }
+        //Specify buffer size in config
+        props.put("batch.size", 16384);
 
+        //Reduce the no of requests less than 0
+        props.put("linger.ms", 1);
 
+        //The buffer.memory controls the total amount of memory available to the ProducerKafka for buffering.
+        props.put("buffer.memory", 33554432);
 
+        props.put("key.serializer",
+                "org.apache.kafka.common.serialization.StringSerializer");
+
+        props.put("value.serializer",
+                "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("auto.create.topics.enable", true);
+
+        return new KafkaProducer<>(props);
+    }
 }
+
+
+
--- src/main/java/kafka/Simulator.java
+++ src/main/java/kafka/Simulator.java
@@ -1,5 +1,34 @@
 package kafka;
 
+import org.apache.kafka.clients.producer.Producer;
+
+import java.io.*;
+import java.util.concurrent.TimeUnit;
+
 public class Simulator {
+    public static final long converterFactor=1000;
+    public static void main(String[] args) throws IOException, InterruptedException {
+        //Read dataset
+        File file = new File("data/"+"CommentsApril2017.csv");
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+        String line,next="";
+        Producer<String, String> producer = ProducerKafka.setConfig();
+        while((line=reader.readLine())!=null){
+            //Produce on Kafka
+            ProducerKafka.produce(producer,line);
+            if((next=reader.readLine())!=null){
+                long firstApproveDate = Long.parseLong(line.split(",", -1)[0]);
+                long nextApproveDate = Long.parseLong(line.split(",", -1)[0]);
+                long time=(nextApproveDate-firstApproveDate); //Difference time between first time and the next
+                TimeUnit.MILLISECONDS.sleep(time);
+                ProducerKafka.produce(producer,next);
+            }
+
+        }
+        producer.close();
+    }
+
+
+
 
 }
